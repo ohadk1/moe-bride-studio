4148313d878822e1d40998b977c1bc1c6d6537f4
ohadk1 <kienanohad3@gmail.com>
2025-04-22 22:09:46 +0300

    ד

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -10,7 +10,7 @@ const Logo = ({ small = false }: LogoProps) => {
     <Link to="/" className="flex items-center">
       <div className={`transition-all duration-300 ${small ? 'w-32' : 'w-48'}`}>
         <img
-          src="/lovable-uploads/54a5aac8-0195-4889-aa6f-65f63e267b8f.png"
+          src="/לבן ללא רקע.png"
           alt="M.O.E bride studio"
           className="w-full h-auto"
         />
